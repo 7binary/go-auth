8afb29a5a21122b6d9460d4f1a1f6e0ea5c2f748
Artem Zinoviev <7binary@gmail.com>
2024-10-13 20:15:37 +0300

    Updated build github-action

--- middlewares/checkAuth.go
+++ middlewares/checkAuth.go
@@ -19,7 +19,7 @@ func CheckAuth(c *gin.Context) {
 	authHeader := c.GetHeader("Authorization")
 
 	if authHeader == "" {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header is missing"})
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header is missing!"})
 		c.AbortWithStatus(http.StatusUnauthorized)
 		return
 	}
